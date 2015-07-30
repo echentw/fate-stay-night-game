1cab51097c77a03cca7c04ca7d18211fc59d98c7
Eric Chen <chen1994tw@gmail.com>
2015-07-29 23:35:29 -0400

    preliminary slashing

--- saber.py
+++ saber.py
@@ -16,34 +16,45 @@ class Saber(object):
     self.image = None                 # the current image of saber to display
 
     # handle when to update the frames
+    self.redraw = False
     self.animate_timer = 0.0
     self.animate_fps = 10.0
+
+    # handle the state of Saber
+    self.slashing = False
 
     # handle directions
     self.direction = pg.K_RIGHT       # the direction Saber is facing
     self.old_direction = None         # the previous direction
     self.direction_stack = []
 
+    # handle which frame to display
+    self.frame_id = 0
+
     # handle walking frames
     self.walk_rect = pg.Rect(walk_rect)
     self.walk_frames = self.get_walk_frames(
         walk_im, [[i, 1] for i in xrange(6)], self.walk_rect)
-    self.walk_id = 0
 
     # handle slashing frames
-#    self.slash_rect = pg.Rect(slash_rect)
-#    self.slash_frames = self.get_slash_frames(
-#        slash_im, [[0,0]], slash_rect)
+    self.slash_rect = pg.Rect(slash_rect)
+    self.slash_frames = self.get_slash_frames(
+        slash_im, [[0,0]], self.slash_rect)
 
-    self.adjust_images()             # initialize the first image
+    self.adjust_images()              # initialize the first image
 
+
+  # Handle keypresses
   def handle_keydown(self, key):
     if key in Saber.DIRECT_DICT:
       if key in self.direction_stack:
         self.direction_stack.remove(key)
       self.direction_stack.append(key)
       self.direction = key
+    elif key == pg.K_e:
+      self.slashing = True
 
+  # Handle key releases
   def handle_keyup(self, key):
     if key in Saber.DIRECT_DICT:
       if key in self.direction_stack:
@@ -64,20 +75,27 @@ class Saber(object):
   def draw(self, surface):
     surface.blit(self.image, self.walk_rect)
 
+  # Helper method for update()
   def adjust_images(self):
-    redraw = False
-    if self.direction != self.old_direction:
+    if self.slashing:
+      self.curr_frames = self.slash_frames[self.direction]
+      self.redraw = True
+    elif self.direction != self.old_direction:
       self.curr_frames = self.walk_frames[self.direction]
       self.old_direction = self.direction
-      redraw = True
+      self.redraw = True
     now = pg.time.get_ticks()
-    if redraw or now - self.animate_timer > 1000 / self.animate_fps:
-      if self.direction_stack:
-        self.walk_id = (self.walk_id + 1) % len(self.curr_frames)
-      self.image = self.curr_frames[self.walk_id]
+    if self.redraw or now - self.animate_timer > 1000 / self.animate_fps:
+      if self.direction_stack or self.slashing:
+        self.frame_id = (self.frame_id + 1) % len(self.curr_frames)
+        self.slashing = False
+      else:
+        self.curr_frames = self.walk_frames[self.direction]
+      self.image = self.curr_frames[self.frame_id]
       self.animate_timer = now
+      self.redraw = False
 
-  # Helper method
+  # Helper method to load walk frames
   def get_walk_frames(self, walk_im, indices, rect):
     sheet = pg.image.load(walk_im).convert()
     sheet.set_colorkey(Saber.COLOR_KEY)
@@ -85,6 +103,15 @@ class Saber(object):
     frame_dict = {pg.K_LEFT : [frames[i] for i in xrange(6)],
                   pg.K_RIGHT: [pg.transform.flip(frames[i], True, False) \
                       for i in xrange(6)]}
+    return frame_dict
+
+  # Helper method to load slash frames
+  def get_slash_frames(self, slash_im, indices, rect):
+    sheet = pg.image.load(slash_im).convert()
+    sheet.set_colorkey(Saber.COLOR_KEY)
+    frames = get_images(sheet, indices, rect.size)
+    frame_dict = {pg.K_LEFT : [frames[0]],
+                  pg.K_RIGHT: [pg.transform.flip(frames[0], True, False)]}
     return frame_dict
 
 
